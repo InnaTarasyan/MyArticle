efcc87900f33d9fea42b9e0c2cff6e77d762f3f4
Inna Tarasyan <innatarasyanmail@gmail.com>
2017-09-06 20:56:06 +0500

    added seeder

---
+++ database/seeds/ArticlesTableSeeder.php
@@ -0,0 +1,58 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ArticlesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('articles')->insert([
+            'title' => str_random(5),
+            'description' => str_random(10),
+            'main_image' => str_random(5).'png',
+            'data' => date("Y-m-d H:i:s"),
+            'url' => 'http'.str_random(5).'.com'
+        ], [
+            'title' => str_random(5),
+            'description' => str_random(10),
+            'main_image' => str_random(5).'png',
+            'data' => date("Y-m-d H:i:s"),
+            'url' => 'http'.str_random(5).'.com'
+        ], [
+            'title' => str_random(5),
+            'description' => str_random(10),
+            'main_image' => str_random(5).'png',
+            'data' => date("Y-m-d H:i:s"),
+            'url' => 'http'.str_random(5).'.com'
+        ], [
+            'title' => str_random(5),
+            'description' => str_random(10),
+            'main_image' => str_random(5).'png',
+            'data' => date("Y-m-d H:i:s"),
+            'url' => 'http'.str_random(5).'.com'
+        ], [
+            'title' => str_random(5),
+            'description' => str_random(10),
+            'main_image' => str_random(5).'png',
+            'data' => date("Y-m-d H:i:s"),
+            'url' => 'http'.str_random(5).'.com'
+        ], [
+            'title' => str_random(5),
+            'description' => str_random(10),
+            'main_image' => str_random(5).'png',
+            'data' => date("Y-m-d H:i:s"),
+            'url' => 'http'.str_random(5).'.com'
+        ], [
+            'title' => str_random(5),
+            'description' => str_random(10),
+            'main_image' => str_random(5).'png',
+            'data' => date("Y-m-d H:i:s"),
+            'url' => 'http'.str_random(5).'.com'
+        ]);
+    }
+}

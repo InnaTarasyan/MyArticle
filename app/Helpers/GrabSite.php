b23d1043c1b285099d62efa3c994dc73a3c2d81a
Inna Tarasyan <innatarasyanmail@gmail.com>
2017-09-09 03:23:03 +0500

    some testing on grabbing data from tert.am using xpath technologies

--- app/Helpers/GrabSite.php
+++ app/Helpers/GrabSite.php
@@ -9,9 +9,74 @@
 class GrabSite
 {
     public function getData(){
-        $img = Image::make('http://www.tert.am/news_images/826/2477835_1/8e32f749936471e696b1802b2d391fd7_3131.jpg');
-        $path= storage_path('article/d.jpg');
-        File::isDirectory($path) or  File::makeDirectory(storage_path('article/'), 0777, true, true);
-        $img->save($path);
+
+        // This is going to be a crone job !!!
+        
+        $url = "http://www.tert.am/";
+        $html = file_get_contents( $url );
+
+        $dom = new DOMDocument();
+        @$dom->loadHTML( $html );
+        $xpath = new DOMXPath( $dom );
+
+        $hrefs= $xpath->query( '//p[@class="today-title"]//a/@href' );
+
+        if( $hrefs ) {
+
+                // url of article
+                $artUrl = $hrefs[0]->nodeValue;
+
+                echo $artUrl.'</br>';
+                $artHtml = file_get_contents( $artUrl );
+
+                $artDom = new DOMDocument();
+                @$artDom->loadHTML( $artHtml );
+                $artXpath = new DOMXPath( $artDom);
+
+                $h= $artXpath->query( '//div[@id="item"]//h1' );
+
+                //title of article
+                if($h){
+                    foreach ($h as $ref) {
+                        echo $ref->nodeValue.'<br/>';
+                    }
+                }
+
+                //date
+                $h= $artXpath->query( '//p[@class="n-d"]' );
+                if($h){
+                    foreach ($h as $ref) {
+                        echo $ref->nodeValue.'<br/>';
+                    }
+                }
+
+                //image
+                $h= $artXpath->query( '//div[@class="i-content"]//img/@src' );
+
+                if($h){
+                    foreach ($h as $ref) {
+                        echo $ref->nodeValue.'<br/>';
+                        $img = Image::make($ref->nodeValue);
+                        $path= storage_path('article/'.substr($artUrl, -7).'.jpg');
+                        File::isDirectory($path) or  File::makeDirectory(storage_path('article/'), 0777, true, true);
+                        $img->save($path);
+                    }
+                }
+
+
+            // article text
+            $h= $artXpath->query( '//div[@class="i-content"]//p' );
+
+            if($h){
+                foreach ($h as $ref) {
+                    echo $ref->nodeValue.'<br/>';
+
+                }
+            }
+
+
+        }
+
+
     }
 }

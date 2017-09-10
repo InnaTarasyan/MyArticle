be1fd4c32c58696c6b70d1d1150dedbe9fa557fd
Inna Tarasyan <innatarasyanmail@gmail.com>
2017-09-10 23:41:38 +0500

    removed testing data

--- database/seeds/ArticlesTableSeeder.php
+++ database/seeds/ArticlesTableSeeder.php
@@ -11,12 +11,5 @@ class ArticlesTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('articles')->insert([
-            'title' => str_random(5),
-            'description' => str_random(10),
-            'main_image' => str_random(5).'png',
-            'data' => date("Y-m-d H:i:s"),
-            'url' => 'http'.str_random(5).'.com'
-        ]);
     }
 }
